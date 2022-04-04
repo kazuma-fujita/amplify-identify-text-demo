bc2df379667014679ffeba9d294437a72a305fff
kazuma-fujita <kazuma.fujita.zuma@gmail.com>
2022-04-04 18:57:34 +0900

    first commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifyidentifytextd13f5e3f3": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "predictions": {
+        "identifyText33ceff53": {
+            "region": "string",
+            "format": "string"
+        }
+    }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,106 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Predictions, {
+  AmazonAIPredictionsProvider,
+} from "@aws-amplify/predictions";
+import Amplify from "aws-amplify";
+import { useCallback, useRef, useState } from "react";
+import Webcam from "react-webcam";
+import awsconfig from "./aws-exports";
 
-function App() {
+Amplify.configure(awsconfig);
+Amplify.addPluggable(new AmazonAIPredictionsProvider());
+
+const videoConstraints = {
+  width: 720,
+  height: 360,
+  facingMode: "user",
+};
+
+const Camera = () => {
+  const [isCaptureEnable, setCaptureEnable] = useState<boolean>(false);
+  const [response, setResponse] = useState("Please capture image.");
+  const webcamRef = useRef<Webcam>(null);
+  const [url, setUrl] = useState<string | null>(null);
+  const capture = useCallback(() => {
+    const imageSrc = webcamRef.current?.getScreenshot();
+    if (imageSrc) {
+      setUrl(imageSrc);
+      Predictions.identify({
+        text: {
+          source: {
+            bytes: imageSrc,
+          },
+          format: "ALL",
+        },
+      })
+        .then((response) => {
+          const {
+            text: {
+              // same as PLAIN + FORM + TABLE
+              fullText,
+            },
+          } = response;
+          setResponse(fullText);
+        })
+        .catch((err) => console.log({ err }));
+    }
+  }, [webcamRef]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h1>Camera</h1>
       </header>
-    </div>
+      <div>result: {response}</div>
+      {isCaptureEnable || (
+        <button onClick={() => setCaptureEnable(true)}>Launch</button>
+      )}
+      {isCaptureEnable && (
+        <>
+          <div>
+            <button onClick={() => setCaptureEnable(false)}>Finish</button>
+          </div>
+          <div>
+            <Webcam
+              audio={false}
+              width={540}
+              height={360}
+              ref={webcamRef}
+              screenshotFormat="image/jpeg"
+              videoConstraints={videoConstraints}
+            />
+          </div>
+          <button onClick={capture}>Capture</button>
+        </>
+      )}
+      {url && (
+        <>
+          <div>
+            <button
+              onClick={() => {
+                setUrl(null);
+              }}
+            >
+              Delete
+            </button>
+          </div>
+          <div>
+            <img src={url} alt="Screenshot" width={540} height={360} />
+          </div>
+        </>
+      )}
+    </>
   );
-}
+};
 
-export default App;
+const IndexPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Amplify Textract Demo</title>
+      </Head>
+      <Camera />
+    </>
+  );
+};
+
+export default IndexPage;
